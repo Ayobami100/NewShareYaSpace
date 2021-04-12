ca567b6a208393ea3db581ce5e6517766142ea3c
deewhy <quadrijamiu2014@gmail.com>
2021-04-12 17:14:19 +0100

    Registration fixed

--- js/main.js
+++ js/main.js
@@ -2,24 +2,189 @@
 // axios.defaults.baseURL="https://share.highflierstutors.com/api/";
 
 
+
 axios.defaults.headers.common['Authorization'] = 'Bearer ' + localStorage.getItem('token');
 
+async function loginUser() {
+
+  var email = document.getElementById("email1").value;
+  var password = document.getElementById('password1').value;
+  alert(password);
+  if (email == '') {
+    $('#email').next().show();
+    return false;
+  }
+  // if (IsEmail(email) == false) {
+  //   // $('.formerror').css('display', "block");
+  //   return false;
+  // }
+  if (password == '') {
+    return false
+  }
+  else
+
+  alert(email);
+  await axios.post('https://share.highflierstutors.com/api/login', {
+    email: "d@gmail.com",
+    password: "1234567890",
+  })
+
+    .then(function (response) {
+      alert(password);
+      alert(email);
+      console.log()
+      localStorage.setItem('token', response.data.token);
+      console.log(response.data)
+      localStorage.setItem('user', JSON.stringify(response.data.user));
+      puser = localStorage.getItem('token');
+      console.log(response.data.user.firstname)
+      // loadIt()
+      clearSignOff();
+      // alert('gggggggggggg')
+      alert('You are Logged in  ' + response.data.user.firstname)
+      window.location.reload();
+
+    })
+
+    .catch(function (error) {
+       alert('You are Logged in  ')
+      // res = error.response.data.message[0];
+      // alert(res)
+    });
+
+
+
+
+  $('.modal , .reg-overlay').fadeOut(200);
+  $("html, body").removeClass("hid-body");
+
+
+}
+
+async function registerUser() {
+
+  var firstname = document.getElementById("firstname").value
+  var lastname = document.getElementById("lastname").value
+  var emailreg = document.getElementById("emailreg").value
+  var country = document.getElementById("country").value
+  var phone = document.getElementById("phone").value
+  var start = document.getElementById("start").value
+  var address = document.getElementById("address").value
+  var password = document.getElementById("password").value
+  // imgProfile = imgProfile
+  var schoolId = "3";
+  var matricNo = document.getElementById("mat").value;
+  var cos = document.getElementById("cos").value;
+
+  if (firstname == '' || lastname == '' || emailreg == '' || country == '' || phone == '' || start == ''
+    || address == '' || password == ''
+  ) {
+
+    return false;
+  }
+  await axios.post('https://share.highflierstutors.com/api/register', {
+    firstname: firstname,
+    lastname: lastname,
+    email: emailreg,
+    country: country,
+    number: phone,
+    DOB: start,
+    address: address,
+    password: password,
+    imgProfile: document.getElementById('imageUrl').innerText,
+    schoolId: schoolId,
+    course: cos,
+    matricNo: matricNo,
+    role: '1',
+    number_verification: '2'
+  })
+    .then(function (response) {
+
+      if (response.data.status == "success") {
+        localStorage.setItem('token', response.data.token);
+        puser = localStorage.getItem('token');
+        document.getElementById("otp").style.display = "block";
+        document.getElementById("profileinfo").style.display = "none";
+        registerResponse = response.data;
+        console.log(response)
+        console.log(response.data)
+        console.log(response.data.data)
+        console.log(puser)
+        console.log(response.data.data.otp)
+        document.getElementById("otpnumber").value = response.data.data.otp;
+      }
+
+
+      // loadIt()
+      // clearOff();
+
+
+
+      // alert('gggggggggggg')
+      alert('You have successfully registered  ' + response.data.data.firstname)
+
+    })
+
+    .catch(function (error) {
+      console.log(error);
+    });
+
+
+}
+function findStudent() {
+  var yes = document.getElementById("yes").checked;
+
+  if (yes == true) {
+    document.getElementById("studentdetails").style.display = "block";
+  }
+  else
+    document.getElementById("studentdetails").style.display = "none";
+
+}
+
+function logOff() {
+  localStorage.removeItem('token');
+  localStorage.removeItem('user')
+
+  loadIt();
+  window.location.reload();
+
+}
 var puser = localStorage.getItem('token');
-// var otpnumber = document.getElementById("otpnumber").value;
- var registerResponse ={};
+
+
+var registerResponse = localStorage.getItem(JSON.parse('user'));
+
 // index.html -----------------------------------------
 
 
 // const axios = require('axios').default;
-function loadIt(){
-  if(puser != null){
-    registerResponse = JSON.parse(localStorage.getItem('user'));
+
+
+function verifyUser() {
+  window.location.href = 'index.html';
+}
+function loadIt() 
+{
+  if (puser != null) {
+
+
     console.log(registerResponse)
+    console.log(puser)
+    
     document.getElementById('signout').style.display = 'block';
     document.getElementById('signin').style.display = 'none';
-     document.getElementById('userValue').innerText = "Hello, "+ registerResponse.firstname+ " "+registerResponse.lastname;
     document.getElementById('reg').style.display = 'none';
     document.getElementById('addlisting').style.display = 'block';
+
+
+    // console.log(localStorage.getItem('user'))
+    // if(localStorage.getItem('user') == Object){
+     
+      document.getElementById('userValue').innerText = "Hello, " + registerResponse.firstname + " " + registerResponse.lastname;
+     
+    // }
+   
     // document.getElementById('userValue').innerText = " ";
     // document.getElementById("studentdetails").style.display = "none";
 
@@ -27,22 +192,22 @@ function loadIt(){
     // $("#start").attr("min",d.getFullYear() + "-01-01").attr("max",d.getFullYear() + "-12-31");
     // alert(d)
 
-      // var $dp2 = $("#datepicker2");
-      // $dp2.datepicker({
-      //   changeYear: true,
-      //   changeMonth: true,
-      //   yearRange: "-100:+20",
-      //   dateFormat: "yy-m-dd",
-      // });
-  
-     
+    // var $dp2 = $("#datepicker2");
+    // $dp2.datepicker({
+    //   changeYear: true,
+    //   changeMonth: true,
+    //   yearRange: "-100:+20",
+    //   dateFormat: "yy-m-dd",
+    // });
 
-    // document.getElementById("otp").style.display = "none";
+
+    console.log('otp')
+    document.getElementById("otp").style.display = "none";
     // populateCombo()
   }
-    else{
-    
-      document.getElementById('signout').style.display = 'none';
+  else {
+
+    document.getElementById('signout').style.display = 'none';
     document.getElementById('signin').style.display = 'block';
     document.getElementById('dropit').style.display = 'block';
     // document.getElementById('avatar').src = res.data.user.imgProfile;
@@ -51,264 +216,133 @@ function loadIt(){
 
     // document.getElementById('userValue').innerText = "Hello,";
     // document.getElementById("studentdetails").style.display = "none";
-   
-   
-    document.getElementById("otp").style.display = "none";
-    }
-}
 
-///////////////////////////////////////////////////////////
-function logOff(){
-    localStorage.removeItem('token'); 
+   
+      document.getElementById("otp").style.display = "none";
+  }
+}
+  function clearSignOff() {
+    document.getElementById("email").value = '';
+    document.getElementById("password").value = '';
+  }
+
   
-    loadIt();
-    window.location.reload();
-   
-}
-
-////////////////////////////////////////////////////////////////
-function clearSignOff(){
-  document.getElementById("email").value='';
-  document.getElementById("password").value = '';
-}
-function clearOff(){
-  document.getElementById("email").value='';
-  document.getElementById("password").value = '';
-document.getElementById("firstname").value = '';
-   document.getElementById("lastname").value = '';
- document.getElementById("emailreg").value = '';
-  document.getElementById("country").value = '';
-   document.getElementById("phone").value = '';
- document.getElementById("start").value = '';
-  document.getElementById("address").value = '';
- document.getElementById("password").value = '';
-  // imgProfile = imgProfile
- document.getElementById("school").value = '';
-  document.getElementById("mat").value = '';
-document.getElementById("cos").value = '';
-}
+  function clearOff() {
+    document.getElementById("email").value = '';
+    document.getElementById("password").value = '';
+    document.getElementById("firstname").value = '';
+    document.getElementById("lastname").value = '';
+    document.getElementById("emailreg").value = '';
+    document.getElementById("country").value = '';
+    document.getElementById("phone").value = '';
+    document.getElementById("start").value = '';
+    document.getElementById("address").value = '';
+    document.getElementById("password").value = '';
+    // imgProfile = imgProfile
+    document.getElementById("school").value = '';
+    document.getElementById("mat").value = '';
+    document.getElementById("cos").value = '';
+  }
 
 
-function assignValues(){
+  function assignValues() {
     // document.getElementById('loader').style.display='none';
     document.getElementById('avatar').src = userDetails.user.imgProfile;
-    document.getElementById('userValue').innerText ="Hello,"+" "+ userDetails.user.firstname;
+    document.getElementById('userValue').innerText = "Hello," + " " + userDetails.user.firstname;
     // document.getElementById('username').innerText = response.login;
     // document.getElementById('location').innerText = response.location;
     // document.getElementById('bio').innerText = response.bio;
     // document.getElementById('count').innerText = "Foll0wers : "+response.followers;
-}
-function IsEmail(email) {
-  var regex = /^([a-zA-Z0-9_\.\-\+])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
-  if(!regex.test(email)) {
-     return false;
-  }else{
-     return true;
   }
-}
-async function loginUser(){
-
-    var email = document.getElementById("email").value;
-    var password = document.getElementById('password').value;
-    if(email== ''){
-      $('#email').next().show();
+  function IsEmail(email) {
+    var regex = /^([a-zA-Z0-9_\.\-\+])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+    if (!regex.test(email)) {
       return false;
-   }
-   if(IsEmail(email)==false){
-    // $('.formerror').css('display', "block");
-    return false;
+    } else {
+      return true;
+    }
   }
-  if(password = ''){
-    return false
-  }
-   await axios.post('https://share.highflierstutors.com/api/login', {
-        email: email,
-        password: password
-      } )
+ 
+  
+
+  // /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+  async function getUserReview() {
+    await axios.get('https://share.highflierstutors.com/api/review')
+
       .then(function (response) {
-          localStorage.setItem('token', response.data.token);
-          localStorage.setItem('user', JSON.stringify(response.data.user));
-          puser = localStorage.getItem('token');
-          console.log(response.data.user.firstname)
-          // loadIt()
-          clearSignOff();
-        // alert('gggggggggggg')
-        alert('You are Logged in  ' + response.data.user.firstname)
-        window.location.reload();
-    
-      })
-     
-      .catch(function (error) {
-        // alert('You are not Logged in  '+ error.)
-        res = error.response.data.message[0];
-        alert(res)
-      });
+        console.log(response);
+        console.log(response.data.length);
+      }
+      )
+  }
+  async function getUserListing() {
+    await axios.get('https://share.highflierstutors.com/api/listing')
 
-     
-     
-   
-      $('.modal , .reg-overlay').fadeOut(200);
-        $("html, body").removeClass("hid-body");
-    
- 
+      .then(function (response) {
+        console.log(response);
+        console.log(response.data.length);
+      }
+      )
+  }
+  async function getUserBooking() {
+
   }
 
-// /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  async function loadReview() {
 
-async function getUserReview(){
-  await axios.get('https://share.highflierstutors.com/api/review')
+    loadIt();
 
-  .then(function (response) {
-    console.log(response);
-    console.log(response.data.length);
-    } 
-   )
-}
-async function getUserListing(){
-  await axios.get('https://share.highflierstutors.com/api/listing')
 
-  .then(function (response) {
-  console.log(response);
-  console.log(response.data.length);
-   } 
-   )
-}
-async function getUserBooking(){
-
-}
-
-async function loadReview(){
-    
-  loadIt();
- 
-   
-    }
- function verifyUser() {
-  window.location.href = 'index.html';
- }
-  async function registerUser(){
-    
-    var firstname =  document.getElementById("firstname").value 
-    var lastname =  document.getElementById("lastname").value 
-    var emailreg =  document.getElementById("emailreg").value 
-    var country =  document.getElementById("country").value 
-    var phone =  document.getElementById("phone").value 
-    var start =  document.getElementById("start").value 
-    var address =  document.getElementById("address").value 
-    var password =  document.getElementById("password").value 
-    // imgProfile = imgProfile
-    var schoolId =  "3";
-    var matricNo =  document.getElementById("mat").value ;
-    var cos =        document.getElementById("cos").value ;
-   
-    if(firstname == '' || lastname == '' || email== '' || emailreg=='' || country == '' || phone == '' || start == ''
-    || address == '' || password ==''
-    ){
-   
-      return false;
-   }
-    await axios.post('https://share.highflierstutors.com/api/register', {
-firstname : firstname,
-lastname: lastname,
-email:emailreg,
-country: country,
-number:phone,
-DOB:start,
-address:address,
-password:password,
-imgProfile: document.getElementById('imageUrl').innerText,
-schoolId: schoolId,
-course:cos,
-matricNo:matricNo,
-role:'1',
-number_verification:'2'
-  })
-  .then(function (response) {
-
-    if(response.data.status == "success"){
-      localStorage.setItem('token', response.data.token);
-      puser = localStorage.getItem('token');
-      document.getElementById("otp").style.display = "block";
-      document.getElementById("profileinfo").style.display = "none";
-      registerResponse = response.data;
-      console.log(response)
-      console.log(response.data)
-      console.log(response.data.data)
-      console.log(puser)
-      console.log(response.data.data.otp)
-      document.getElementById("otpnumber").value = response.data.data.otp;
-    }
-    
-
-    // loadIt()
-    // clearOff();
-   
-   
-
-  // alert('gggggggggggg')
-  alert('You have successfully registered  ' + response.data.data.firstname)
-
-})
-
-.catch(function (error) {
-  console.log(error);
-});
-
-    
   }
+  
+ 
 
 
-    ////////////////////////////////////////////////////////////////////////////////////////
-    function findStudent(){
-      var yes = document.getElementById("yes").checked;
+  ////////////////////////////////////////////////////////////////////////////////////////
+ 
+  ///////////////////////////////////////////////////////
 
-      if (yes==true){
-        document.getElementById("studentdetails").style.display = "block";
-      }
-      else
-        document.getElementById("studentdetails").style.display = "none";
-      
-    }
- ///////////////////////////////////////////////////////
+  function populateCombo() {
 
- function populateCombo(){
-
-  let dropdown = document.getElementById('locality-dropdown');
-  console.log(dropdown)
-dropdown.length = 0;
+    let dropdown = document.getElementById('locality-dropdown');
+    console.log(dropdown)
+    dropdown.length = 0;
 
 
-let defaultOption = document.createElement('option');
-defaultOption.text = 'Choose Category';
+    let defaultOption = document.createElement('option');
+    defaultOption.text = 'Choose Category';
 
-dropdown.add(defaultOption);
-dropdown.selectedIndex = 0;
+    dropdown.add(defaultOption);
+    dropdown.selectedIndex = 0;
 
-axios.get('https://share.highflierstutors.com/api/allCategory')
+    axios.get('https://share.highflierstutors.com/api/allCategory')
 
-.then(function (response) {
-      if (response.status !== 200) {  
-        console.warn('Looks like there was a problem. Status Code: ' + 
-          response.status);  
-        return;  
-      }
+      .then(function (response) {
+        if (response.status !== 200) {
+          console.warn('Looks like there was a problem. Status Code: ' +
+            response.status);
+          return;
+        }
 
-      // Examine the text in the response  
-      // console.log(response);
-      // .then(function(data) {  
+        // Examine the text in the response  
+        // console.log(response);
+        // .then(function(data) {  
         let option;
-        
-    console.log(response)
-    	for (let i = 0; i < response.data.data.length; i++) {
-          option = document.createElement('option');
-      	  option.text = response.data.data[i].categoryName;
-      	  option.value = response.data.data[i].id;
-      	  dropdown.add(option);
-    	
-      }}
- )
 
-  .catch(function(err) {  
-    console.error('Fetch Error -', err);  
-  });
- }
+        console.log(response)
+        for (let i = 0; i < response.data.data.length; i++) {
+          option = document.createElement('option');
+          option.text = response.data.data[i].categoryName;
+          option.value = response.data.data[i].id;
+          dropdown.add(option);
+
+        }
+      }
+      )
+
+      .catch(function (err) {
+        console.error('Fetch Error -', err);
+      });
+  }
+}
